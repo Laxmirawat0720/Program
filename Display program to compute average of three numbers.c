bb3f2a3c0b5337d9a5857050bedc6c62eb53d197
Laxmirawat0720 <155939431+Laxmirawat0720@users.noreply.github.com>
2024-02-16 12:08:37 +0530

    Add files via upload

---
+++ Display program to compute average of three numbers.c
@@ -0,0 +1,13 @@
+   #include<stdio.h>
+   #include<conio.h>
+   void main()
+   {
+   int a,b,c;
+   float d;
+   clrscr();
+   printf("enter three number a,b,c");
+   scanf("%d%d%d",&a,&b,&c);
+   d=(a+b+c)/3.0;
+   printf("average=%f",d);
+   getch();
+   }
